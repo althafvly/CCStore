611b9b5a9424c8e7853b041954df5c986c9121c1
althafvly <althafvly@gmail.com>
2025-07-05 21:41:24 +0530

    split apk build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,6 +17,15 @@ android {
         versionName = "1.0"
     }
 
+    splits {
+        abi {
+            isEnable = true
+            reset()
+            include("armeabi-v7a", "arm64-v8a", "x86", "x86_64")
+            isUniversalApk = false
+        }
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = true
