90d88f29bbad3088342984c1714e2e720eb7b366
althafvly <althafvly@gmail.com>
2025-07-07 21:26:53 +0530

    Bump version to v1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.sorrybro.ccstore"
         minSdk = 33
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
     }
 
     splits {
